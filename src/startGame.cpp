8c7904db7d4be58a188e96a6ed1291ce8d34b93d
Joan Torres <jtguevara585@hotmail.com>
2022-03-29 21:05:16 -0400

    Added source file
    (startGame.cpp) Added source file that will be used to implement the game

---
+++ src/startGame.cpp
@@ -0,0 +1,23 @@
+/*
+FILE: startGame.cpp
+DESCRIPTION: A function definition.
+*/
+
+#include <forward_list>
+
+void startGame(){
+	//TO DO
+	
+    //Declare game data:
+		//Player, WordList, Timer, score, letterCount, points
+    //Fill word list
+    //WHILE player has not lost(Timer = 0) or quit:
+        //Retrieve a word from the word list
+        //Scramble word and give to player to solve
+        //Set timer
+        //Prompt player to solve(unscramble) and type correct word:
+		// WHILE word is not solved:
+			//> Prompt user to type again:
+       //Calculate and give points
+    //End game
+}

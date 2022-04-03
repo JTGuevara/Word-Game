ca1386c30919442fa313ff74bba4b94e7a6eff36
Joan Torres <jtguevara585@hotmail.com>
2022-04-02 21:08:14 -0400

    Implemented player logic
    (startGame.cpp) Implemented player logic. Added documentation for included libraries.

--- src/startGame.cpp
+++ src/startGame.cpp
@@ -3,30 +3,35 @@ FILE: startGame.cpp
 DESCRIPTION: A function definition.
 */
 
-#include <forward_list> //STL data structure for word list
-#include <fstream> //C++ IO library for retrieving words from a file
-#include <random>
-#include <algorithm>
-#include <cassert>
+#include <forward_list> 	//STL data structure for word list
+#include <fstream> 			//C++ IO library for retrieving words from a file
+#include <algorithm> 		//For shuffling algorithm(std::shuffle) used to scramble words
+#include <random> 			//For random number generator(std::random_device) and number engine(std::mt19937) used in shuffling algorithm
+#include <cassert> 			//For testing preconditions.
 
 #include "GameTimer.hpp"
-std::string scrambleWord(std::string);//Scrambles the given word and returns a copy. Returns an error if word is empty or less than 2 letters.
+std::string scrambleWord(std::string word);
+//Scrambles the given word and returns a copy. Returns an error if word is empty or less than 2 letters.
+
 
 void startGame(){
 	////////TO DO///////
 	
     //Declare game data:
-		//Player, WordList, Timer, score, letterCount, points
+		//Player, WordList, Timer, score, letterCount, points, etc.
 	std::forward_list<std::string> WordList;
+	const std::size_t LIST_SIZE = 5;
 	std::string word;
 	std::string scrambledWord;
+	std::size_t points = 0;
 	GameTimer timer;
+	std::string playerResponse;
 	
     //Fill word list
 	{
 		//local variables used to read from a file
 		int i = 0;
-		std::string wordsRead[5];
+		std::string wordsRead[LIST_SIZE];
 		std::ifstream file;
 		
 		//file directory - make sure to change this if the file is moved!
@@ -66,16 +71,39 @@ void startGame(){
 		}
 		timer.endTimer();
 		timer.printTime();
+		timer.startTimer();
+		
         //Prompt player to solve(unscramble) and type correct word:
+		std::cout << "\n\n\nPLAYER TEST\n----------------";
+		std::cout << "\nWord: " << scrambledWord; 
+		std::cout << "\nResponse: ";
+		std::cin >> playerResponse;
+		
 		// WHILE word is not solved:
+		while(playerResponse != word){
+			timer.tick();
 			//> Prompt user to type again:
+			std::cout << "\nNope! Try again!\n";
+			std::cout << "\nResponse: ";
+			std::cin >> playerResponse;
+		}
        //Calculate and give points
+	   std::cout << "\n\nCorrect!\n";
+	   points += word.length() * 100;
+	   std::cout << "\n- - - - - - - -";
+	   std::cout << "\nScore: " << points << " pts"; 
+	   std::cout << "\n- - - - - - - -";
+	   
+	   timer.endTimer();
+	   timer.printTime();
+	
+	std::cout << "\nGame over!";
     //End game
 }
 
 std::string scrambleWord(std::string word){
 	//check for valid word
-	assert(word.length() >= 2 && word != "");
+	assert(word.length() >= 2 || word != "");
 	
 	std::string scrambledWord = word;
 	
@@ -86,3 +114,5 @@ std::string scrambleWord(std::string word){
 	std::shuffle(scrambledWord.begin(), scrambledWord.end(), randomizingAlgorithm);
 	return scrambledWord;
 }
+
+
